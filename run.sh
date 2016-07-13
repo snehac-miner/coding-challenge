f98df67d09206b783463a26767c39f079ecc17eb
snehachalla1991 <snehachalla.2k3@gmail.com>
2016-07-13 15:47:52 -0400

    testing

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 
 # Run this script on your terminal from coding_challenge folder
 
-python src/rolling_median.py venmo_input/venmo-trans.txt  venmo_output/output.txt
 
-#python ./src/rolling_median.py ./venmo_input/venmo-trans.txt ./venmo_output/output.txt
+
+python ./src/rolling_median.py ./venmo_input/venmo-trans.txt ./venmo_output/output.txt
 
